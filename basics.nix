037f37d3ee4872a45c95cc17310e4a72d253eb65
Ray Ashman Jr <ray@rayashmanjr.com>
2020-08-09 19:28:47 -0400

    More basics

--- basics.nix
+++ basics.nix
@@ -1,6 +1,16 @@
 { config, lib, pkgs, ... }:
 
 {
+  programs.zsh = {
+    enable = true;
+    history.extended = true;
+
+    initExtra = ''
+      source ~/.nix-profile/etc/profile.d/nix.sh
+      export PROMPT="%B%F{magenta}%n@%m%f [%F{blue}%~%f]%b ⚡  "
+    '';
+  };
+
   home.packages = with pkgs; [
     ripgrep
     fira-code
